48f3e97283508dcf08575f74d65d0c6cd7ac3947
victor <Krsj2ageiG5n>
2021-10-21 17:02:33 +0200

    [TASK] Update TodoTask Model

--- app/app/Models/TodoTask.php
+++ app/app/Models/TodoTask.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class TodoTask extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function creator() {
+        return $this->hasOne(User::class);
+    }
+
+    public function assignedTo() {
+        return $this->hasMany(User::class);
+    }
 }
